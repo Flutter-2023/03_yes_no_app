e4cc1c32851a7543e9fffd511ff3c265b67a89b1
Luis Angel <angelfgdeveloper@gmail.com>
2023-05-10 21:19:27 -0500

    Inicializando proyecto basico

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,25 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+
+  const MyApp({ super.key });
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Yes No App',
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Material App Bar'),
         ),
-        body: const Center(
-          child: Text('Hello World'),
+        body: Center(
+          child: FilledButton.tonal(
+            onPressed: () {}, 
+            child: const Text('Click me'),
+          ),
         ),
       ),
     );
