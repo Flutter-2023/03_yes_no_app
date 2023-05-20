8bed4082476ae52fdd8a6b45e946c9abe095d47e
Luis Angel <angelfgdeveloper@gmail.com>
2023-05-19 23:57:47 -0500

    Comportamiento del FormField

--- lib/presentation/widgets/shared/message_field_box.dart
+++ lib/presentation/widgets/shared/message_field_box.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 
 class MessageFieldBox extends StatelessWidget {
-
-  const MessageFieldBox({ super.key });
+  const MessageFieldBox({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final textController = TextEditingController();
+
+    // Elemento a poner el foco
+    final focusNode = FocusNode();
 
     //final colors = Theme.of(context).colorScheme;
 
@@ -15,31 +18,41 @@ class MessageFieldBox extends StatelessWidget {
     );
 
     final inputDecoration = InputDecoration(
-        // enabledBorder: OutlineInputBorder(
-        //   borderSide: BorderSide(
-        //     color: colors.primary
-        //   ),
-        //   borderRadius: BorderRadius.circular(20)
-        // ),
-        enabledBorder: outlineInputBorder,
-        focusedBorder: outlineInputBorder,
-        filled: true,
-        suffixIcon: IconButton(
-          icon: const Icon(Icons.send_outlined),
-          onPressed: () {
-            print('Valor de la caja de texto');
-          },
-        ),
-      );
+      // enabledBorder: OutlineInputBorder(
+      //   borderSide: BorderSide(
+      //     color: colors.primary
+      //   ),
+      //   borderRadius: BorderRadius.circular(20)
+      // ),
+      hintText: 'End your message with a "??"',
+      enabledBorder: outlineInputBorder,
+      focusedBorder: outlineInputBorder,
+      filled: true,
+      suffixIcon: IconButton(
+        icon: const Icon(Icons.send_outlined),
+        onPressed: () {
+          final textValue = textController.value.text;
+          print('button $textValue');
+          textController.clear();
+        },
+      ),
+    );
 
     return TextFormField(
+      onTapOutside: (event) {
+        focusNode.unfocus(); // Cuando hago click afuera quita ek foco
+      },
+      focusNode: focusNode,
+      controller: textController,
       decoration: inputDecoration,
       onFieldSubmitted: (value) {
         print('Submit $value');
+        textController.clear();
+        focusNode.requestFocus(); // cuando limpia mantiene el foco
       },
-      onChanged: (value) {
-        print('Changed $value');
-      },
+      // onChanged: (value) {
+      //   print('Changed $value');
+      // },
     );
   }
 }
